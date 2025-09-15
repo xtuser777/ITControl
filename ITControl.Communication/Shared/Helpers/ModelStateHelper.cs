ec44f5112ae4314a0b70817dc725cfb9049271c9
Lucas Oliveira <lucaoxt@gmail.com>
2025-09-15 20:57:10 -0300

    delete unecessary helpers

--- ITControl.Communication/Shared/Helpers/ModelStateHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-
-namespace ITControl.Communication.Shared.Helpers;
-
-public static class ModelStateHelper
-{
-    public static ModelStateDictionary ModelState { get; set; } = null!;
-}
