a0dc996ba683a7c659861de57560d60d4085fef8
Lucas Oliveira <lucaoxt@gmail.com>
2025-10-27 22:24:10 -0300

    Pages seed

--- ITControl.Infrastructure/Pages/EntitiesConfiguration/PageConfiguration.cs
+++ ITControl.Infrastructure/Pages/EntitiesConfiguration/PageConfiguration.cs
@@ -6,6 +6,25 @@ namespace ITControl.Infrastructure.Pages.EntitiesConfiguration;
 
 public class PageConfiguration : IEntityTypeConfiguration<Page>
 {
+    internal static readonly IEnumerable<Page> PagesSeed = new List<Page>([
+        new Page(new () { Name = "users" }),
+        new Page(new () { Name = "roles" }),
+        new Page(new () { Name = "pages" }),
+        new Page(new () { Name = "positions" }),
+        new Page(new () { Name = "departments" }),
+        new Page(new () { Name = "divisions" }),
+        new Page(new () { Name = "contracts" }),
+        new Page(new () { Name = "equipments" }),
+        new Page(new () { Name = "systems" }),
+        new Page(new () { Name = "calls" }),
+        new Page(new () { Name = "appointments" }),
+        new Page(new () { Name = "treatments" }),
+        new Page(new () { Name = "notifications" }),
+        new Page(new () { Name = "knowledge-bases" }),
+        new Page(new () { Name = "profile" }),
+        new Page(new () { Name = "supplements" }),
+        new Page(new () { Name = "supplements-movements" }),
+    ]);
     public void Configure(EntityTypeBuilder<Page> builder)
     {
         builder.HasKey(t => t.Id);
@@ -13,5 +32,7 @@ public class PageConfiguration : IEntityTypeConfiguration<Page>
         builder.Property(x => x.CreatedAt).IsRequired();
         builder.Property(x => x.UpdatedAt).IsRequired();
         builder.HasIndex(p => p.Name).IsUnique();
+        
+        builder.HasData(PagesSeed);
     }
 }
